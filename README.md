c92650bdc6ff43bb9ad0c711464f85d2a85181ba
Clinton Montague <clinton@slightlymore.co.uk>
2010-12-12 14:47:13 +0000

    Setting up the repo

---
+++ README.md
@@ -0,0 +1,6 @@
+TODO: Write documentation!
+
+Basic examples can be found in 'example.html'
+
+## Depends on
+* [jquery](https://github.com/jquery/jquery)
---
+++ jquery.jiggle.js
@@ -0,0 +1,38 @@
+(function ($) {
+	$.fn.jiggle = function (options) {
+		settings = $.extend ({
+				speed: 150,
+				distance: 3
+			}, options);
+		return this.each (function () {
+			var $this = $(this),
+				do_jiggle = false,
+				random_jump = function () {
+					var sign = Math.round (Math.random()),
+						number = Math.floor (Math.random() * settings.distance);
+					if (sign) return number + 'px';
+					return -number + 'px';
+				},
+				start_jiggle = function () {
+					if (!$('html').hasClass('no-csstransitions')) return;
+					var time = settings.speed;
+					if (do_jiggle)
+					{
+						$this.css({position:'relative'})
+							.animate({left:random_jump(), top:random_jump()}, time/4)
+							.animate({left:random_jump(), top:random_jump()}, time/2)
+							.animate({left:random_jump(),  top:random_jump()}, time/4);
+						setTimeout (start_jiggle, time);
+					}
+				},
+				stop_jiggle = function () {
+					do_jiggle = false;
+					$this.stop();
+				};
+			$this.hover (function () {
+					do_jiggle = true;
+					start_jiggle();
+				}, stop_jiggle)
+		});
+	};
+})(jQuery);
